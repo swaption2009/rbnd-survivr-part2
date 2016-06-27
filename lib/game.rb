adee507aade0c092dc4fb2113212c78e10ef5406
swaption2009 <swaption2009@gmail.com>
2016-06-27 14:11:34 -0700

    complete game test

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,30 @@
 class Game
-  attr_accessor :tribes
+  attr_reader :tribes
 
-  def initialize
-    @tribes = []
+  def initialize(*tribe)
+    @tribes = tribe
   end
 
   def add_tribe(tribe)
     @tribes << tribe
+  end
+
+  def immunity_challenge
+    @tribes.sample
+  end
+
+  def clear_tribes
+    @tribes.clear
+  end
+
+  def merge(merged_tribe_name)
+    merged_members = @tribes[0].members + @tribes[1].members
+    clear_tribes
+    add_tribe(Tribe.new({name: merged_tribe_name, members: merged_members}))
+    return @tribes.first
+  end
+
+  def individual_immunity_challenge
+    @tribes.sample.members.sample
   end
 end
